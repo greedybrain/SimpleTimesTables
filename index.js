39cdd0d7bbcbc883cf67b73447522f80cf8b3e2a
Naya Willis <greedybrain18@gmail.com>
2020-04-12 15:42:49 -0400

    created a short simple JS Library to do simple multiplication

---
+++ index.js
@@ -0,0 +1,47 @@
+class TimesTable {
+
+     static double(num) {
+          return num * num;
+     }
+
+     static triple(num) {
+          return num * 3
+     }
+
+     static quad(num) {
+          return num * 4
+     }
+
+     static quin(num) {
+          return num * 5
+     }
+
+     static sext(num) {
+          return num * 6
+     }
+
+     static sept(num) {
+          return num * 7
+     }
+
+     static octu(num) {
+          return num * 8
+     }
+
+     static nonu(num) {
+          return num * 9
+     }
+
+     static decu(num) {
+          return num * 10
+     }
+
+     static unde(num) {
+          return num * 11
+     }
+
+     static duod(num) {
+          return num * 12
+     }
+
+}
